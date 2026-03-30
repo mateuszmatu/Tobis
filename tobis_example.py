ffa281d1d35af7eb62638939f3a3962027b39b24
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2026-03-30 11:54:12 +0000

    updated example

--- tobis_example.py
+++ tobis_example.py
@@ -1,11 +1,11 @@
 from run_opendrift import run_opendrift
 import numpy as np
 from datetime import datetime, timedelta
-start_date = datetime(2026,3,1,6)
-end_date = datetime(2026,3,1,9)
+start_date = datetime(2026,3,2,0)
+end_date = datetime(2026,3,4,0)
 
 times = np.arange(start_date, end_date, timedelta(minutes=60)).astype(datetime)
-times = '2025-05-03T00:00:00'
+#times = '2026-03-03T00:00:00'
 
 lon = 5.35
 lat = 57
@@ -24,14 +24,14 @@ run_opendrift(
     #radius=100000,
     #geojson='tobis/tobis_singlepart.geojson',
     #rls = 'tobis/particles_example.rls',
-    netCDF='test_geojson_2025-05-01T00:00:00.nc',
+    netCDF='test1.nc',
     z = -5,
     duration=48,
     time_step=60,
     time_step_output=60,
-    outfile=f'test_geojson_{times}.nc',
-    #horizontal_diffusivity=0,
-    #coastline='Model',
+    outfile=f'test2.nc',
+    traj_time_index=24, 
+    coastline='Model',
     vertical_mixing=False,
     N=1,
     particle_type='LarvalFish',
