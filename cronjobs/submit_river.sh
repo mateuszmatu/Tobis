adfd31eec51bf29eb08710847ea054388a40d8dd
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2026-05-07 09:59:57 +0000

    increased resource need with more rivers

--- cronjobs/submit_river.sh
+++ cronjobs/submit_river.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #$ -S /bin/bash
-#$ -l h_rt=2:00:00
+#$ -l h_rt=4:00:00
 #$ -q bigmem-r8.q
-#$ -l h_rss=30G
-#$ -l mem_free=30G 
-#$ -l h_data=30G
+#$ -l h_rss=50G
+#$ -l mem_free=50G 
+#$ -l h_data=50G
 #$ -o /lustre/storeB/users/mateuszm/thredds/oper/cronlog/output/
 #$ -e /lustre/storeB/users/mateuszm/thredds/oper/cronlog/error/
 
